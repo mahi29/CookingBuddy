df8669177173ca4d26a260d84644f51ee59a11d9
Mahith Amancherla <mahi29@berkeley.edu>
2014-02-28 00:19:00 -0800

    Created the home page XML

--- src/group/cs169/cookingbuddy/MainActivity.java
+++ src/group/cs169/cookingbuddy/MainActivity.java
@@ -1,8 +1,9 @@
 package group.cs169.cookingbuddy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +19,16 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/** Called when the 'Sign Up' button is clicked from Home Screen
+	 */
+	public void signUp(View v) {
+		
+	}
+	/** Called when the 'Log In' button is clicked from the Home Screen*/
+	public void logIn(View v) {
+		
 	}
 
 }
