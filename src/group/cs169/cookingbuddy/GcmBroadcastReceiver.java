b21b3c68e5028a1c3cc7d35dc7876f9c216fc5dc
Mahith Amancherla <mahi29@berkeley.edu>
2014-05-01 14:52:19 -0700

    Receiving GCM notifications

---
+++ src/group/cs169/cookingbuddy/GcmBroadcastReceiver.java
@@ -0,0 +1,26 @@
+package group.cs169.cookingbuddy;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+
+	public GcmBroadcastReceiver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// Explicitly specify that GcmIntentService will handle the intent.
+		ComponentName comp = new ComponentName(context.getPackageName(),
+				GcmIntentService.class.getName());
+		// Start the service, keeping the device awake while it is launching.
+		startWakefulService(context, (intent.setComponent(comp)));
+		setResultCode(Activity.RESULT_OK);
+
+	}
+
+}
